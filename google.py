4a8c38a0c601d87520ca011aa107faf9e64820f7
猫川七海 <109388848+NekokawaNanami@users.noreply.github.com>
2022-10-27 13:49:08 +0900

    Google翻訳に対応

--- google.py
+++ google.py
@@ -2,14 +2,15 @@ import discord
 import os
 import requests
 from dotenv import load_dotenv
+from googletrans import Translator
 load_dotenv()
 
 Token = os.getenv('Token')
 source = int(os.getenv('source_channel'))
 target = int(os.getenv('target_channel'))
-DeepLToken = os.getenv('DeepLToken')
 intents = discord.Intents.all() 
 client = discord.Client(intents=intents)
+translator = Translator()
 
 @client.event
 async def on_ready():
@@ -27,43 +28,43 @@ async def on_message(message):
     else:
         try:
             if message.channel == source_channel:
-                request = requests.get(f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=EN")
+                request = requests.get(f"https://script.google.com/macros/s/AKfycbzowZjdWrs8td1cnJNwjmaVuSmpfR6gpYYQHNnJ6cPHDVedJXtv1K65CWtlZZ0SSgBGHQ/exec?text={message.content}&source=ja&target=en")
                 result = request.json()
                 webhooks = await target_channel.webhooks()
                 webhook = webhooks[0]
             elif message.channel == target_channel:
-                request = requests.get(f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=JA")
+                request = requests.get(f"https://script.google.com/macros/s/AKfycbzowZjdWrs8td1cnJNwjmaVuSmpfR6gpYYQHNnJ6cPHDVedJXtv1K65CWtlZZ0SSgBGHQ/exec?text={message.content}&source=en&target=ja")
                 result = request.json()
                 webhooks = await source_channel.webhooks()
                 webhook = webhooks[0]
             if message.attachments:
                 for attachment in message.attachments:
-                    await webhook.send(content=result["translations"][0]["text"]+ "\n" + attachment.url,
+                    await webhook.send(content=result["text"]+ "\n" + attachment.url,
                                 username=message.author.name,
                                 avatar_url=message.author.avatar.url
                                 )
             else:
-                await webhook.send(content=result["translations"][0]["text"],
+                await webhook.send(content=result["text"],
                                 username=message.author.name,
                                 avatar_url=message.author.avatar.url
                                 )
         except Exception:
             if message.channel == source_channel:
-                request = requests.get(f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=EN")
+                request = requests.get(f"https://script.google.com/macros/s/AKfycbzowZjdWrs8td1cnJNwjmaVuSmpfR6gpYYQHNnJ6cPHDVedJXtv1K65CWtlZZ0SSgBGHQ/exec?text={message.content}&source=ja&target=en")
                 result = request.json()
                 webhook = source_channel.create_webhook(name="英語")
             elif message.channel == target_channel:
-                request = requests.get(f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=JA")
+                request = requests.get(f"https://script.google.com/macros/s/AKfycbzowZjdWrs8td1cnJNwjmaVuSmpfR6gpYYQHNnJ6cPHDVedJXtv1K65CWtlZZ0SSgBGHQ/exec?text={message.content}&source=en&target=ja")
                 result = request.json()
                 webhook = source_channel.create_webhook(name="日本語")
             if message.attachments:
                 for attachment in message.attachments:
-                    await webhook.send(content=result["translations"][0]["text"]+ "\n" + attachment.url,
+                    await webhook.send(content=result["text"] + "\n" + attachment.url,
                                 username=message.author.name,
                                 avatar_url=message.author.avatar.url
                                 )
             else:
-                await webhook.send(content=result["translations"][0]["text"],
+                await webhook.send(content=result["text"],
                                 username=message.author.name,
                                 avatar_url=message.author.avatar.url
                                 )
