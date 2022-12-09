ead6dc107e66fad5edc260f9efd88bd530bd539b
猫川七海 <mail@nekokawa.net>
2022-12-10 02:27:50 +0900

    WebhookをWickとかに消されても勝手に作り直してくれるように更新

--- google.py
+++ google.py
@@ -2,12 +2,13 @@ import discord
 import os
 import requests
 from dotenv import load_dotenv
+
 load_dotenv()
 
 Token = os.getenv('Token')
 source = int(os.getenv('source_channel'))
 target = int(os.getenv('target_channel'))
-intents = discord.Intents.all() 
+intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 @client.event
@@ -19,31 +20,39 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.bot:
-        return
-    elif message.author.discriminator == "0000":
+    if message.author.bot or message.author.discriminator == "0000":
         return
     else:
         if message.channel == source_channel:
-            webhooks = await target_channel.webhooks()
-            webhook = webhooks[0]
-            request = requests.get(f"https://script.google.com/macros/s/AKfycbzowZjdWrs8td1cnJNwjmaVuSmpfR6gpYYQHNnJ6cPHDVedJXtv1K65CWtlZZ0SSgBGHQ/exec?text={message.content}&source=ja&target=en")
-            result = request.json()
+            try:
+                webhooks = await target_channel.webhooks()
+                webhook = webhooks[0]
+                r = requests.get(f"https://script.google.com/macros/s/AKfycbzowZjdWrs8td1cnJNwjmaVuSmpfR6gpYYQHNnJ6cPHDVedJXtv1K65CWtlZZ0SSgBGHQ/exec?text={message.content}&source=ja&target=en")
+                result = r.json()
+            except Exception:
+                webhook = await target_channel.create_webhook(name="英語")
+                r = requests.get(f"https://script.google.com/macros/s/AKfycbzowZjdWrs8td1cnJNwjmaVuSmpfR6gpYYQHNnJ6cPHDVedJXtv1K65CWtlZZ0SSgBGHQ/exec?text={message.content}&source=ja&target=en")
+                result = r.json()
         elif message.channel == target_channel:
-            webhooks = await source_channel.webhooks()
-            webhook = webhooks[0]
-            request = requests.get(f"https://script.google.com/macros/s/AKfycbzowZjdWrs8td1cnJNwjmaVuSmpfR6gpYYQHNnJ6cPHDVedJXtv1K65CWtlZZ0SSgBGHQ/exec?text={message.content}&source=en&target=ja")
-            result = request.json()
+            try:
+                webhooks = await source_channel.webhooks()
+                webhook = webhooks[0]
+                r = requests.get(f"https://script.google.com/macros/s/AKfycbzowZjdWrs8td1cnJNwjmaVuSmpfR6gpYYQHNnJ6cPHDVedJXtv1K65CWtlZZ0SSgBGHQ/exec?text={message.content}&source=en&target=ja")
+                result = r.json()
+            except Exception:
+                webhook = await source_channel.create_webhook(name="日本語")
+                r = requests.get(f"https://script.google.com/macros/s/AKfycbzowZjdWrs8td1cnJNwjmaVuSmpfR6gpYYQHNnJ6cPHDVedJXtv1K65CWtlZZ0SSgBGHQ/exec?text={message.content}&source=en&target=ja")
+                result = r.json()
         if message.attachments:
             for attachment in message.attachments:
-                await webhook.send(content=result["text"]+ "\n" + attachment.url,
-                            username=message.author.name,
-                            avatar_url=message.author.avatar.url
-                            )
+                await webhook.send(content=result["text"] + "\n" + attachment.url,
+                                   username=message.author.name,
+                                   avatar_url=message.author.avatar.url
+                                   )
         else:
             await webhook.send(content=result["text"],
-                            username=message.author.name,
-                            avatar_url=message.author.avatar.url
-                            )
+                               username=message.author.name,
+                               avatar_url=message.author.avatar.url
+                               )
 
 client.run(Token)
--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import discord
 import os
 import requests
 from dotenv import load_dotenv
+
 load_dotenv()
 
 Token = os.getenv('Token')
 source = int(os.getenv('source_channel'))
 target = int(os.getenv('target_channel'))
 DeepLToken = os.getenv('DeepLToken')
-intents = discord.Intents.all() 
+intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 @client.event
@@ -20,32 +21,42 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.bot:
-        return
-    elif message.author.discriminator == "0000":
+    if message.author.bot or message.author.discriminator == "0000":
         return
     else:
         if message.channel == source_channel:
-            webhooks = await target_channel.webhooks()
-            webhook = webhooks[0]
-            request = requests.get(f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=EN")
-            result = request.json()
-            source_channel.create_webhook(name="英語")
+            try:
+                webhooks = await target_channel.webhooks()
+                webhook = webhooks[0]
+                r = requests.get(
+                    f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=EN")
+                result = r.json()
+            except Exception:
+                webhook = await target_channel.create_webhook(name="英語")
+                r = requests.get(f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=EN")
+                result = r.json()
         elif message.channel == target_channel:
-            webhooks = await source_channel.webhooks()
-            webhook = webhooks[0]
-            request = requests.get(f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=JA")
-            result = request.json()
+            try:
+                webhooks = await source_channel.webhooks()
+                webhook = webhooks[0]
+                r = requests.get(
+                    f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=JA")
+                result = r.json()
+            except Exception:
+                webhook = await source_channel.create_webhook(name="日本語")
+                r = requests.get(
+                    f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=JA")
+                result = r.json()
         if message.attachments:
             for attachment in message.attachments:
-                await webhook.send(content=result["translations"][0]["text"]+ "\n" + attachment.url,
-                            username=message.author.name,
-                            avatar_url=message.author.avatar.url
-                            )
+                await webhook.send(content=result["translations"][0]["text"] + "\n" + attachment.url,
+                                   username=message.author.name,
+                                   avatar_url=message.author.avatar.url
+                                   )
         else:
             await webhook.send(content=result["translations"][0]["text"],
-                            username=message.author.name,
-                            avatar_url=message.author.avatar.url
-                            )
+                               username=message.author.name,
+                               avatar_url=message.author.avatar.url
+                               )
 
 client.run(Token)
