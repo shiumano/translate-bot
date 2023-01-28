f5d2e128f44d5c3c8b1406921e5ac428410861cb
猫川七海 <mail@nekokawa.net>
2023-01-29 05:19:05 +0900

    あ

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def on_message(message):
             try:
                 webhooks = await target_channel.webhooks()
                 webhook = webhooks[0]
-            except Exception:
+            except TypeError:
                 webhook = await target_channel.create_webhook(name="英語")
             r = requests.get(f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=EN")
             result = r.json()
@@ -42,20 +42,20 @@ async def on_message(message):
             try:
                 webhooks = await source_channel.webhooks()
                 webhook = webhooks[0]
-            except Exception:
+            except TypeError:
                 webhook = await source_channel.create_webhook(name="日本語")
             r = requests.get(f"https://api-free.deepl.com/v2/translate?auth_key={DeepLToken}&text={message.content}&target_lang=JA")
             result = r.json()
         if message.attachments:
             for attachment in message.attachments:
                 await webhook.send(content=result["translations"][0]["text"] + "\n" + attachment.url,
-                                   username=message.author.name,
-                                   avatar_url=message.author.avatar.url
+                                   username=message.author.display_name,
+                                   avatar_url=message.author.display_avatar.url
                                    )
         else:
             await webhook.send(content=result["translations"][0]["text"],
-                               username=message.author.name,
-                               avatar_url=message.author.avatar.url
+                               username=message.author.display_name,
+                               avatar_url=message.author.display_avatar.url
                                )
 
 
